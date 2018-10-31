850944f8e31477c4ddc1dcada78dd3b8ca1c4d53
dan-fern <d.carlosfernandez@gmail.com>
2018-10-31 14:52:38 -0700

    Prior to this commit, the project files were not included.  After this commit, project files are included and ready for submission.

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -1,6 +1,7 @@
 #ifndef FusionEKF_H_
 #define FusionEKF_H_
 
+
 #include "measurement_package.h"
 #include "Eigen/Dense"
 #include <vector>
@@ -9,41 +10,44 @@
 #include "kalman_filter.h"
 #include "tools.h"
 
-class FusionEKF {
+
+class FusionEKF
+{
+
 public:
-  /**
-  * Constructor.
-  */
-  FusionEKF();
 
-  /**
-  * Destructor.
-  */
-  virtual ~FusionEKF();
+    /** Constructor */
+    FusionEKF( );
 
-  /**
-  * Run the whole flow of the Kalman Filter from here.
-  */
-  void ProcessMeasurement(const MeasurementPackage &measurement_pack);
+    /** Destructor */
+    virtual ~FusionEKF( );
 
-  /**
-  * Kalman Filter update and prediction math lives in here.
-  */
-  KalmanFilter ekf_;
+    /** Run the whole flow of the Kalman Filter from here */
+    void ProcessMeasurement( const MeasurementPackage &measurement_pack );
+
+    // Kalman Filter update and prediction math lives in here
+    KalmanFilter ekf_;
 
 private:
-  // check whether the tracking toolbox was initialized or not (first measurement)
-  bool is_initialized_;
 
-  // previous timestamp
-  long long previous_timestamp_;
+    // check whether tracking toolbox was initialized or not (first measurement)
+    bool is_initialized_;
 
-  // tool object used to compute Jacobian and RMSE
-  Tools tools;
-  Eigen::MatrixXd R_laser_;
-  Eigen::MatrixXd R_radar_;
-  Eigen::MatrixXd H_laser_;
-  Eigen::MatrixXd Hj_;
+    // previous timestamp
+    long long previous_timestamp_;
+
+    // tool object used to compute Jacobian and RMSE
+    Tools tools;
+
+    Eigen::MatrixXd R_laser_;
+
+    Eigen::MatrixXd R_radar_;
+
+    Eigen::MatrixXd H_laser_;
+
+    Eigen::MatrixXd Hj_;
+
 };
+
 
 #endif /* FusionEKF_H_ */
--- src/tools.h
+++ src/tools.h
@@ -3,32 +3,31 @@
 #include <vector>
 #include "Eigen/Dense"
 
+
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using namespace std;
 
-class Tools {
+
+class Tools
+{
+
 public:
-  /**
-  * Constructor.
-  */
-  Tools();
 
-  /**
-  * Destructor.
-  */
-  virtual ~Tools();
+    /** Constructor */
+    Tools( );
 
-  /**
-  * A helper method to calculate RMSE.
-  */
-  VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+    /** Destructor */
+    virtual ~Tools( );
 
-  /**
-  * A helper method to calculate Jacobians.
-  */
-  MatrixXd CalculateJacobian(const VectorXd& x_state);
+    /** A helper method to calculate RMSE */
+    VectorXd CalculateRMSE( const vector<VectorXd> &estimations,
+                            const vector<VectorXd> &ground_truth );
+
+    /** A helper method to calculate Jacobians. */
+    MatrixXd CalculateJacobian( const VectorXd& x_state );
 
 };
+
 
 #endif /* TOOLS_H_ */
